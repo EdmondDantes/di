a19ae55889c0c56bd83802eff3f081dcb3a9dd06
Edmond <1571649+EdmondDantes@users.noreply.github.com>
2024-07-28 17:20:37 +0300

    + Binding

---
+++ src/Binding.php
@@ -0,0 +1,20 @@
+<?php
+declare(strict_types=1);
+
+namespace IfCastle\DI;
+
+use Attribute;
+
+/**
+ * Indicates that this class is bound to the specified interfaces.
+ */
+#[Attribute(Attribute::TARGET_CLASS)]
+final readonly class Binding
+{
+    public readonly array $interfaces;
+    
+    public function __construct(string ...$interfaces)
+    {
+        $this->interfaces           = $interfaces;
+    }
+}
