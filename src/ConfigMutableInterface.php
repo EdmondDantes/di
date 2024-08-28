9335ca2c4077848beacc0fcfca3ae1203d3c045c
Edmond <1571649+EdmondDantes@users.noreply.github.com>
2024-08-28 17:52:50 +0300

    + ConfigMutableInterface

---
+++ src/ConfigMutableInterface.php
@@ -0,0 +1,23 @@
+<?php
+declare(strict_types=1);
+
+namespace IfCastle\DI;
+
+interface ConfigMutableInterface extends ConfigInterface
+{
+    public function set(string $node, mixed $value): static;
+    
+    public function setSection(string $node, array $value): static;
+    
+    public function merge(array $config): static;
+    
+    public function mergeSection(string $node, array $config): static;
+    
+    public function remove(string ...$path): static;
+    
+    public function reset(): static;
+    
+    public function asImmutable(): static;
+    
+    public function cloneAsMutable(): static;
+}
