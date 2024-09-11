672c02f749c5203e7aff958550b8fd115cfb02da
Edmond <1571649+EdmondDantes@users.noreply.github.com>
2024-09-11 17:18:32 +0300

    Extend ContainerMutableInterface from ContainerInterface
    This change ensures that ContainerMutableInterface now inherits methods from ContainerInterface. The update promotes better consistency and enables polymorphism within the container implementations.

--- src/ContainerMutableInterface.php
+++ src/ContainerMutableInterface.php
@@ -3,7 +3,7 @@ declare(strict_types=1);
 
 namespace IfCastle\DI;
 
-interface ContainerMutableInterface
+interface ContainerMutableInterface extends ContainerInterface
 {
     public function set(string $key, mixed $value): static;
     
