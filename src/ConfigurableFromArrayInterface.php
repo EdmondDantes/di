f3cf0e69edf9d09eb15f84ef58b0451df6fe8280
Edmond <1571649+EdmondDantes@users.noreply.github.com>
2024-09-16 21:37:09 +0300

    + ConfigurableFromArrayInterface

--- src/ConfigurableFromArrayInterface.php
+++ src/ConfigurableFromArrayInterface.php
@@ -3,6 +3,13 @@ declare(strict_types=1);
 
 namespace IfCastle\DI;
 
+/**
+ * Interface ConfigurableFromArrayInterface
+ *
+ * Used to apply configuration from an array.
+ *
+ * @package IfCastle\DI
+ */
 interface ConfigurableFromArrayInterface
 {
     public function applyConfigArray(array $config): void;
