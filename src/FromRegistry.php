8d5e3c36f2d51641d3071a0479d0ed794697c89e
Edmond <1571649+EdmondDantes@users.noreply.github.com>
2024-11-01 19:03:46 +0200

    + add ConfigAwareInterface.php + add ConfigurationProviderInterface.php % refactoring FromRegistry.php

--- src/FromRegistry.php
+++ src/FromRegistry.php
@@ -6,6 +6,17 @@ namespace IfCastle\DI;
 
 use Attribute;
 
+/**
+ * ## FromRegistry attribute.
+ *
+ * This attribute provides configuration based on the Registry.
+ * The Registry is a special type of system configuration, distinct from the usual configuration,
+ * and is component-oriented.
+ *
+ * This attribute also considers the special ConfigurationProviderInterface,
+ * which can be implemented on a dependency descriptor.
+ * In this case, the descriptor itself can provide the configuration for initialization.
+ */
 #[Attribute(Attribute::TARGET_PROPERTY | Attribute::TARGET_PARAMETER)]
 final class FromRegistry extends Dependency implements ProviderInterface
 {
@@ -21,6 +32,15 @@ final class FromRegistry extends Dependency implements ProviderInterface
         DescriptorInterface $descriptor,
         ?DependencyInterface $forDependency = null
     ): mixed {
+
+        if ($forDependency instanceof ConfigurationProviderInterface) {
+            $config                 = $forDependency->provideConfiguration();
+
+            if ($config !== null) {
+                return $config;
+            }
+        }
+
         $registry                     = $container->findDependency(ComponentRegistryInterface::class);
 
         if ($registry === null) {
@@ -31,6 +51,10 @@ final class FromRegistry extends Dependency implements ProviderInterface
             throw new \TypeError('Registry is not an instance of ' . ComponentRegistryInterface::class);
         }
 
-        return $registry->findComponentConfig($this->getDependencyKey());
+        if ($forDependency === null) {
+            return null;
+        }
+
+        return $registry->findComponentConfig($forDependency->getDependencyName());
     }
 }
